c36541b208d8573c9af3760f55cfeb92c11572ce
Вячеслав Семёнов <slavasemenov007@gmail.com>
2026-06-20 17:26:43 +0300

    feat(factory): create Factory for user and workspace creating

--- laravel/database/factories/UserFactory.php
+++ laravel/database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Enums\Role;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -25,12 +26,20 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'username' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
+	    'role' => Role::USER,
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function admin(): static
+    {
+	return $this->state(fn () => [
+	    'role' => Role::ADMIN,
+	]);
     }
 
     /**
---
+++ laravel/database/factories/WorkspaceFactory.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Workspace;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Workspace>
+ */
+class WorkspaceFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->words(2, true),
+	    'owner_id' => User::factory(),
+        ];
+    }
+
+    public function forUser(User $user): static
+    {
+    	return $this->state(fn () => [
+        	'owner_id' => $user->id,
+    	]);
+    }
+}

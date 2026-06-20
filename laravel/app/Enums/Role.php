dceabb0c97640eb641cb127c8545d3ed13661041
Вячеслав Семёнов <slavasemenov007@gmail.com>
2026-06-20 15:49:51 +0300

    feat(models): create User model && enum's roles

---
+++ laravel/app/Enums/Role.php
@@ -0,0 +1,8 @@
+namespace App\Enums;
+
+enum Role: string
+{
+    case USER = 'user';
+    case ADMIN = 'admin';
+    case MODER = 'moder';
+}
--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -9,13 +9,16 @@ use Illuminate\Database\Eloquent\Attributes\Hidden;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Enums\Role;
 
-#[Fillable(['name', 'email', 'password'])]
-#[Hidden(['password', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
     use HasFactory, Notifiable;
+
+    protected $fillable = ['username', 'email', 'password', 'role'];
+
+    protected $hidden = ['password', 'remember_token'];
 
     /**
      * Get the attributes that should be cast.
@@ -27,6 +30,28 @@ class User extends Authenticatable
         return [
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
+ 	    'role' => Role::class,
         ];
+    }
+
+    public function ownedWorkspaces(): HasMany
+    {
+	return $this->hasMany(Workspace::class, 'owner_id'));
+    }
+
+    public function workspaces(): BelongsToMany
+    {
+	return $this->belongsToMany(Workspace::class, 'workspace_members')
+		->withPivot('role')->withTimestamps();
+    }
+
+    public function workspaceMembers(): HasMany
+    {
+	return $this->hasMany(WorkspaceMember::class);
+    }
+
+    public function task(): HasMany
+    {
+	return $this->hasMany(Task::class, 'assigned_to')->latest();
     }
 }

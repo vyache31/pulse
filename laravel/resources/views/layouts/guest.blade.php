a57a58dfe06830d845140165952b01f22a11f197
Вячеслав Семёнов <slavasemenov007@gmail.com>
2026-06-22 08:31:34 +0300

    hotfix: change filename in vite connection and route after register

--- laravel/resources/views/layouts/guest.blade.php
+++ laravel/resources/views/layouts/guest.blade.php
@@ -10,7 +10,7 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     </head>
     <body class="font-sans text-gray-900 antialiased">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
